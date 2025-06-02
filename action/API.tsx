e3537ac5cfa107299f2149d3b04bb4d886bec0ea
pradyuman kumar singh <acp8804@gmail.com>
2025-06-02 10:30:54 +0530

    search api

--- action/API.tsx
+++ action/API.tsx
@@ -1,10 +1,11 @@
-const BASE_URL = "http://192.168.19.200:8000";
+const BASE_URL = "http://127.0.0.1:8000";
 const BASE_API_URL = BASE_URL + "/api";
 
 const ENDPOINTS = {
     list_banners: BASE_API_URL + "/banners",
     list_home_products: BASE_API_URL + "/products/home-products",
     list_product_details: BASE_API_URL + "/products",
+    search_product: BASE_API_URL + "/search"
 };
 
 export { ENDPOINTS };
